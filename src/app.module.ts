d9c6ec68a6972dce2f79c311145ca7399ffe7473
mert <mertcelilozdin@hotmail.com>
2023-06-04 22:09:06 +0300

    joi package added for .env file validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth';
 import { MovieModule } from './movie';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { CoreModule } from './core/core.module';
 import { CommonModule } from './common/common.module';
 import { TicketModule } from './ticket/ticket.module';
@@ -12,7 +13,13 @@ import { WatchModule } from './watch/watch.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        MONGO_URL: Joi.string().required(),
+      }),
+    }),
     MongooseModule.forRoot(process.env.MONGO_URL),
     AuthModule,
     MovieModule,
